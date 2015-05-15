71a1e74f5a842458ed727c1d4135a9db2d323414
ardhena <olgadraber@gmail.com>
2015-05-15 18:11:18 +0200

    set up topic CRUD

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -11,20 +11,33 @@ class TopicsController < ApplicationController
   def new
   end
 
-  def create
-  end
-
   def edit
   end
 
+  def create
+    self.topic = Topic.new(topic_params)
+    if topic.save
+      redirect_to topic, notice: 'Grupa została pomyślnie utworzona.'
+    else
+      render action: 'new'
+    end
+  end
+
   def update
+    if topic.update(topic_params)
+      redirect_to topic, notice: 'Grupa została pomyślnie zaktualizowana.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    topic.destroy
+    redirect_to topics_url, notice: 'Grupa została usunięta.'
   end
 
-  def topic_params
-    params.require(:topic).permit(:name, :description)
-  end
-
+  private
+    def topic_params
+      params.require(:topic).permit(:name, :description)
+    end
 end
