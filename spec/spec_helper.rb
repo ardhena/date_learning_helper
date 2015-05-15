67f214569b9240f6da959039f0615e2ea1e49f2d
ardhena <olgadraber@gmail.com>
2015-05-15 23:30:47 +0200

    add test-database cleaner to fix a bug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,10 +26,18 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
-
   config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
     DatabaseCleaner.clean
   end
 
