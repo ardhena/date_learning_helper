5e797258aaa41f8f6405207bd7bd95f9b1a04e46
ardhena <olgadraber@gmail.com>
2015-05-16 18:37:12 +0200

    add event model validations + specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_length_of(:name).is_at_least(3) }
+  it { should validate_length_of(:name).is_at_most(255) }
+  it { should validate_presence_of(:start_date) }
+  it { should validate_presence_of(:end_date) }
+  it { should belong_to :topic }
 end
