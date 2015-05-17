814092f87d65c7ce6048d77bb02da60cfcf9c579
ardhena <olgadraber@gmail.com>
2015-05-17 23:28:32 +0200

    add learning index action

--- app/controllers/learning_controller.rb
+++ app/controllers/learning_controller.rb
@@ -1,2 +1,12 @@
 class LearningController < ApplicationController
+
+  before_action :authenticate_user!
+
+  expose_decorated(:topics)
+
+  def index
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :events
   end
 
+  get 'learning', to: 'learning#index', as: 'learning_all'
   get 'learning/:id', to: 'learning#show', as: :learning
 
 end
