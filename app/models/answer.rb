87352070a56137019d1f434f9dbe2af5fa6778d2
ardhena <olgadraber@gmail.com>
2015-06-03 13:01:04 +0200

    fix answer to be with date and question object

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,8 +6,16 @@ class Answer
     if args[:end_date].empty?
       args[:end_date] = args[:start_date]
     end
-    args.each do |k,v|
-      instance_variable_set("@#{k}", v) unless v.nil?
+    @start_date = args[:start_date].to_date
+    @end_date = args[:end_date].to_date
+    @question = Event.find(args[:question])
+  end
+
+  def valid?
+    if @start_date == @question.start_date && @end_date == @question.end_date
+      true
+    else
+      false
     end
   end
 end
